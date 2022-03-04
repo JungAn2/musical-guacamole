08be57c7dd17b8d9aef1ed9a3d4a057c532d7d21
JungAn2 <jungan1991@gmail.com>
2022-03-04 18:43:38 -0500

    messing around with menu

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+        <div class="col"/>
+        <div class="col">
+            <div class="">
+                <div class ="row">
+                <q-btn round class="float-left"/>
+                <q-btn round/>
+                </div>
+            </div>
+            <div class="">
+                <q-btn round/>
+            </div>
+        </div>
+        <div class="col"/>
+    </div>
+</template>
